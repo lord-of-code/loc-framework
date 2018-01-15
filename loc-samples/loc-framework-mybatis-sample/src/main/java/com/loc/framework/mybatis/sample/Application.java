96467a9b12264c5af3e35d43bdd111c0e285b7f0
longzhe <dragonlong1986@126.com>
2018-01-15 18:04:13 +0800

    add application

--- loc-samples/loc-framework-mybatis-sample/src/main/java/com/loc/framework/mybatis/sample/Application.java
+++ loc-samples/loc-framework-mybatis-sample/src/main/java/com/loc/framework/mybatis/sample/Application.java
@@ -1,8 +1,13 @@
 package com.loc.framework.mybatis.sample;
 
+import org.springframework.boot.SpringApplication;
+
 /**
  * Created on 2018/1/15.
  */
 public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
 }
