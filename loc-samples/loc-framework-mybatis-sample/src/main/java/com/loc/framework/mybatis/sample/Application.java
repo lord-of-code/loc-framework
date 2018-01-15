fc763a95e073eb4772bb5ca13269b597ec30e2c4
longzhe <dragonlong1986@126.com>
2018-01-15 18:07:08 +0800

    polish

--- loc-samples/loc-framework-mybatis-sample/src/main/java/com/loc/framework/mybatis/sample/Application.java
+++ loc-samples/loc-framework-mybatis-sample/src/main/java/com/loc/framework/mybatis/sample/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
  * Created on 2018/1/15.
  */
 public class Application {
+
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
