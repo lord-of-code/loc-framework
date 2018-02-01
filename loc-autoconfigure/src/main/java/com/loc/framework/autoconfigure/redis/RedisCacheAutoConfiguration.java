6c4a7d042c6b184ac495628d7ab59400e0df7535
longzhe <dragonlong1986@126.com>
2018-02-01 23:50:21 +0800

    增加了部分redis cache value的序列化改成jackson的方式

--- loc-autoconfigure/src/main/java/com/loc/framework/autoconfigure/redis/RedisCacheAutoConfiguration.java
+++ loc-autoconfigure/src/main/java/com/loc/framework/autoconfigure/redis/RedisCacheAutoConfiguration.java
@@ -5,22 +5,15 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.time.Duration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.cache.CacheProperties.Redis;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.cache.RedisCacheWriter;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.lang.Nullable;
 
 /**
  * Created on 2018/2/1.
@@ -28,7 +21,7 @@ import org.springframework.lang.Nullable;
 @Slf4j
 @Configuration
 @ConditionalOnClass(Redis.class)
-public class RedisCacheAutoConfiguration extends CachingConfigurerSupport {
+public class RedisCacheAutoConfiguration {
 
   @Bean
   public RedisCacheConfiguration redisCacheConfiguration() {
@@ -48,44 +41,12 @@ public class RedisCacheAutoConfiguration extends CachingConfigurerSupport {
         .serializeValuesWith(SerializationPair.fromSerializer(jackson2JsonRedisSerializer));
   }
 
-  @Override
-  @Bean
-  public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-
-    return RedisCacheManager.RedisCacheManagerBuilder.fromCacheWriter(new RedisCacheWriter(delegate) {
-
-      private RedisCacheManager delegate;
-
-      public RedisCacheManager
-
-      @Override
-      public void put(String s, byte[] bytes, byte[] bytes1, @Nullable Duration duration) {
-
-      }
-
-      @Nullable
-      @Override
-      public byte[] get(String s, byte[] bytes) {
-        return new byte[0];
-      }
-
-      @Nullable
-      @Override
-      public byte[] putIfAbsent(String s, byte[] bytes, byte[] bytes1,
-          @Nullable Duration duration) {
-        return new byte[0];
-      }
-
-      @Override
-      public void remove(String s, byte[] bytes) {
-
-      }
-
-      @Override
-      public void clean(String s, byte[] bytes) {
-
-      }
-    }).build();
-  }
+//  @Bean
+//  public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+//    CustomRedisCacheWriter customRedisCacheWriter = new CustomRedisCacheWriter(
+//        RedisCacheWriter.lockingRedisCacheWriter(redisConnectionFactory));
+//    return RedisCacheManager.RedisCacheManagerBuilder
+//        .fromCacheWriter(customRedisCacheWriter).build();
+//  }
 
 }
