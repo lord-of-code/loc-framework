b64c8adc2fbf4763fe996ef650c46c0b5f622ef0
longzhe <529039038@qq.com>
2018-04-12 17:36:23 +0800

    增加2个cloud的demo

---
+++ loc-samples/loc-framework-cloud-client-sample/src/main/java/com/loc/framework/cloud/client/sample/Application.java
@@ -0,0 +1,16 @@
+package com.loc.framework.cloud.client.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created on 2017/12/2.
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
