153a6a88e8c0f0d74abf94dd9a1a8ffaa2ede70f
longzhe <529039038@qq.com>
2018-04-19 13:55:37 +0800

    增加配置中心的starter

---
+++ loc-samples/loc-framework-config-sample/src/main/java/com/loc/framework/config/sample/Application.java
@@ -0,0 +1,29 @@
+package com.loc.framework.config.sample;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created on 2017/12/2.
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+
+  @Slf4j
+  @RestController
+  public static class GetController {
+
+    @GetMapping(value = "/hello")
+    public String helloWorld() {
+      return "hello world";
+    }
+  }
+}
