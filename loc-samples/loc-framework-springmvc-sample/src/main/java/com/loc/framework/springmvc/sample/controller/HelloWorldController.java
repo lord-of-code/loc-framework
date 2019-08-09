3e5d73b6b3ff34aab5dcb20b07dfc7f66618e7d3
dragonlong1986 <dragonlong1986@126.com>
2019-08-09 11:43:15 +0800

    补充了一个时间相关的demo

--- loc-samples/loc-framework-springmvc-sample/src/main/java/com/loc/framework/springmvc/sample/controller/HelloWorldController.java
+++ loc-samples/loc-framework-springmvc-sample/src/main/java/com/loc/framework/springmvc/sample/controller/HelloWorldController.java
@@ -1,15 +1,19 @@
 package com.loc.framework.springmvc.sample.controller;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.google.common.collect.Lists;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,11 +31,13 @@ public class HelloWorldController {
   public Demo testGet(
       @Parameter(description = "名称") @RequestParam(name = "name") String name,
       @Parameter(description = "年龄") @RequestParam(name = "age") Integer age,
+      @Parameter(description = "出生时间") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") @RequestParam(name = "birthTime") LocalDateTime birthTime,
       @Parameter(description = "地址") @RequestParam(name = "address") String address
   ) {
     Demo demo = new Demo();
     demo.setName(name);
     demo.setAge(age);
+    demo.setBirthTime(birthTime);
     demo.setAddress(Lists.newArrayList(address));
     return demo;
   }
@@ -40,16 +46,18 @@ public class HelloWorldController {
   @PostMapping(value = "/test/post-form")
   public Demo testPostForm(@Parameter(description = "名称") @RequestParam(name = "name") String name,
       @Parameter(description = "年龄") @RequestParam(name = "age") Integer age,
+      @Parameter(description = "出生时间") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") @RequestParam(name = "birthTime") LocalDateTime birthTime,
       @Parameter(description = "地址") @RequestParam(name = "address") String address) {
     Demo demo = new Demo();
     demo.setName(name);
     demo.setAge(age);
+    demo.setBirthTime(birthTime);
     demo.setAddress(Lists.newArrayList(address));
     return demo;
   }
 
   @Operation(description = "我是一个简单的POST-BODY接口")
-  @PostMapping(value = "/test/post-body")
+  @PostMapping(value = "/test/post-body", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
   public Demo testPostBody(@RequestBody Demo demo) {
     return demo;
   }
@@ -64,6 +72,9 @@ public class HelloWorldController {
     private String name;
     @Schema(description = "年龄")
     private int age;
+    @Schema(description = "出生时间，格式(yyyy-MM-dd HH:mm:ss)")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime birthTime;
     @Schema(description = "地址列表")
     private List<String> address;
   }
